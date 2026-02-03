7eefd459af3f7da652e313c8b60733fea277796e
Jordan Massey <j.massey2278@gmail.com>
2026-02-02 23:03:16 -0600

    changed name to fivesight

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="text-center">
       <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        Hello From Zed :D
+        Fivesight game epic :D
         <Game />
       </header>
     </div>
