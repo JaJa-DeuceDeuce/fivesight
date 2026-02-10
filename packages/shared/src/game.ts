8b29ce6f58f08f87eb7f88cc98cec4cc79399618
Jordan Massey <j.massey2278@gmail.com>
2026-02-10 00:41:17 -0600

    updated types in shared package

--- packages/shared/src/game.ts
+++ packages/shared/src/game.ts
@@ -1,39 +1,36 @@
 import { type } from 'arktype';
 
 /**
- * Board = 4 quadrants 
+ * Board = 4 quadrants
  * 1 quadrant = 3x3 (9) cells
  */
- 
+
  export const PlayerT = type("'black' | 'white'");
  export type Player = typeof PlayerT.infer;
- 
+
  // CELL
  export const CellT = type("null | 'black' | 'white'");
  export type Cell = typeof CellT.infer;
- 
+
  export const CellIndexT = type("0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8");
  export type CellIndex = typeof CellIndexT.infer;
- 
+
  // QUADRANT
  export const QuadrantT = type([CellT, CellT, CellT, CellT, CellT, CellT, CellT, CellT, CellT]);
  export type Quadrant = typeof QuadrantT.infer;
- 
+
  export const QuadrantIndexT = type("0 | 1 | 2 | 3");
  export type QuadrantIndex = typeof QuadrantIndexT.infer;
- 
+
  //BOARD
  export const BoardT = type([QuadrantT, QuadrantT, QuadrantT, QuadrantT]);
  export type Board = typeof BoardT.infer;
- 
+
  export const RotationDirectionT = type("'cw' | 'ccw'");
  export type RotationDirection = typeof RotationDirectionT.infer;
- 
+
  export const PhaseT = type("'place' | 'rotate'");
- export const Phase = typeof PhaseT.infer;
- 
+ export type Phase = typeof PhaseT.infer;
+
  export const StatusT = type("'waiting' | 'active' | 'finished'");
- export const Status = typeof StatusT.infer;
- 
- export const ResultT = PlayerT.or("'draw'");
- export type Result = typeof ResultT.infer;
+ export type Status = typeof StatusT.infer;
